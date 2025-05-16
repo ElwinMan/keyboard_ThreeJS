2802809d5a1d648c176e56fb6fff8a9625b3b046
ELwin Man <man0008@hz.nl>
2025-05-16 18:12:48 +0200

    working single Q key pressdown

--- src/Experience/World/Keycaps.js
+++ src/Experience/World/Keycaps.js
@@ -1,5 +1,6 @@
 import * as THREE from 'three'
 import Experience from '../Experience.js'
+import gsap from 'gsap'
 
 export default class Keycaps
 {
@@ -10,10 +11,15 @@ export default class Keycaps
         this.resources = this.experience.resources
         this.time = this.experience.time
 
+        // State
+        this.isPressed = false
+        this.originalPosition = new THREE.Vector3()
+
         // Resource
         this.resource = this.resources.items.keycapsModel
 
         this.setModel()
+        this.setEventListeners()
     }
 
     setModel()
@@ -23,10 +29,41 @@ export default class Keycaps
         this.model.rotation.y = -Math.PI / 2
         this.model.position.set(-19.7, 1, -5.7)
         this.scene.add(this.model)
+
+        this.originalPosition.copy(this.model.position)
     }
     
-    update()
-    {
-        
+    setEventListeners() {
+        window.addEventListener('keydown', (event) => {
+            if (event.key.toLowerCase() === 'q' && !this.isPressed) {
+                this.isPressed = true
+                this.animateKeycapPress()
+            }
+        })
+
+        window.addEventListener('keyup', (event) => {
+            if (event.key.toLowerCase() === 'q' && this.isPressed) {
+                this.isPressed = false
+                this.animateKeycapRelease()
+            }
+        })
+    }
+
+    animateKeycapPress() {
+        if (!this.model) return
+        gsap.to(this.model.position, {
+            y: this.originalPosition.y - 0.3, // Moves downwards
+            duration: 0.05,
+            ease: "power1.out"
+        })
+    }
+
+    animateKeycapRelease() {
+        if (!this.model) return
+        gsap.to(this.model.position, {
+            y: this.originalPosition.y, // Moves back up
+            duration: 0.05,
+            ease: "power1.out"
+        })
     }
 }
